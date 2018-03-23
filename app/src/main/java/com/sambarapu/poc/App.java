cf9f30ce8a2eb1de1e5fcbf709c70e2b527075cc
Chandra <NIS1521m@Chandra.local>
2018-03-23 09:34:17 -0700

    Making changes to run the ap

--- app/src/main/java/com/sambarapu/poc/App.java
+++ app/src/main/java/com/sambarapu/poc/App.java
@@ -1,4 +1,4 @@
-package com.dmitriydubson.poc;
+package com.sambarapu.poc;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
--- config-server/src/main/java/com/sambarapu/poc/ConfigServer.java
+++ config-server/src/main/java/com/sambarapu/poc/ConfigServer.java
@@ -1,8 +1,7 @@
-package com.dmitriydubson.poc;
+package com.sambarapu.poc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
 
